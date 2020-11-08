fdcac2ca0ab8c19f9add87ce66173a522a535cc7
follow1123 <1050911237@qq.com>
2020-11-08 18:42:32 +0800

    sql小工具完善（2020-11-8 18:42）

--- src/main/java/com/yang/SQLExecutor/util/stringTemplate/STemplate.java
+++ src/main/java/com/yang/SQLExecutor/util/stringTemplate/STemplate.java
@@ -1,19 +1,33 @@
 package com.yang.SQLExecutor.util.stringTemplate;
 
-import java.util.Map;
+import com.yang.SQLExecutor.util.TimeTest;
+import org.junit.Test;
+
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * @auther YF
  * @create 2020-11-07-20:45
+ * 字符串模板工具
  */
 public class STemplate {
 
-    private static Pattern valueReg =  Pattern.compile("\\{(\\w{1,9})}");
+    //匹配普通值的正则表达式
+    private static Pattern valueReg = Pattern.compile("\\@\\{(\\w{1,9})}");
 
+    //匹配集合的正则表达式
+    private static Pattern collectionReg = Pattern.compile("@\\{(\\w+):\\(([\\w =@\\{}',-]+)\\)([\\.\\w '\\(\\);,:]+)}");
+    //匹配集合内前缀后缀的正则
+    private static Pattern collectionArgReg = Pattern.compile("\\.([jlf])\\('([\\w ;,:\\)\\(]+)'\\)");
+
+    //StringBuilder
     private static StringBuilder sb = new StringBuilder();
-
+    //临时的map主要存数组的下标及对应的值或map的key和value
+    private static Map<String, Object> tempMap = new HashMap<>();
+    //临时参数储存主要储存集合内前缀后缀的值
+    private static Map<String, Object> paramMap = new HashMap<>();
     /**
      * 替换倒数长度的字符串
      *
@@ -23,7 +37,6 @@ public class STemplate {
     private static void replace(int lastLength, String str) {
         sb.replace(sb.length() - lastLength, sb.length(), str);
     }
-
     /**
      * 替换倒数长度的字符串,默认替换为空字符
      *
@@ -32,7 +45,6 @@ public class STemplate {
     private static void replace(int lastLength) {
         replace(lastLength, " ");
     }
-
     /**
      * 返回当前建造器里面的字符串并清空字符串建造器
      *
@@ -44,14 +56,146 @@ public class STemplate {
         return value;
     }
 
-    public static String render(String str, Map<String, String> value){
-        String[] split = str.split(valueReg.toString());
-        Matcher matcher = valueReg.matcher(str);
-        for (int i = 0; i < split.length && matcher.find(); i++) {
-            sb.append(split[i]).append(value.get(matcher.group(1)));
+    /**
+     * 存数组的下标及对应的值
+     * @param index
+     * @param value
+     */
+    public static void putListValue(int index, Object value) {
+        tempMap.put("i", index);
+        tempMap.put("v", value);
+    }
+
+    /**
+     * 存map的key和value
+     * @param key
+     * @param value
+     */
+    public static void putMapValue(String key, Object value) {
+        tempMap.put("k", key);
+        tempMap.put("v", value);
+    }
+
+    /**
+     * 清空临时map
+     */
+    public static void clearMap() {
+        tempMap.clear();
+    }
+
+    /**
+     * 对象为空这置为空字符串
+     * @param o
+     * @return
+     */
+    public static String nullToEmpty(Object o) {
+        return o == null ? "" : o.toString();
+    }
+
+    /**
+     * 渲染普通值得模板字符串
+     * @param target
+     * @param value
+     * @return
+     */
+    public static String renderString(String target, Map<String, Object> value) {
+        String[] split = target.split(valueReg.toString());
+        Matcher matcher = valueReg.matcher(target);
+        String[] sp2 = new String[split.length + 1];
+        sp2[sp2.length - 1] = "";
+        System.arraycopy(split, 0, sp2, 0, split.length);
+
+        for (int i = 0; i < sp2.length; i++) {
+            sb.append(sp2[i]).append(matcher.find() ? nullToEmpty(value.get(matcher.group(1))) : "");
         }
         return value();
     }
 
+    /**
+     * 渲染list的模板
+     * @param target
+     * @param values
+     * @return
+     */
+    public static String renderList(String target, List<Object> values) {
+        String first = nullToEmpty(paramMap.get("f"));
+        String last = nullToEmpty(paramMap.get("l"));
+        String join = nullToEmpty(paramMap.get("j"));
+        sb.append(first);
+        for (int i = 0; i < values.size(); i++) {
+            putListValue(i, values.get(i));
+            sb.append(renderString(target, tempMap)).append(join);
+        }
+        replace(join.length());
+        sb.append(last);
+        clearMap();
+        return value();
+    }
+
+    /**
+     * 渲染map的模板
+     * @param target
+     * @param values
+     * @return
+     */
+    public static String renderMap(String target, Map<String, Object> values) {
+        String first = nullToEmpty(paramMap.get("f"));
+        String last = nullToEmpty(paramMap.get("l"));
+        String join = nullToEmpty(paramMap.get("j"));
+        sb.append(first);
+        values.forEach((k, v) -> {
+            putMapValue(k, v);
+            sb.append(renderString(target, tempMap)).append(join);
+        });
+        replace(join.length());
+        sb.append(last);
+        clearMap();
+        return value();
+    }
+
+    /**
+     * 渲染传入的模板字符串
+     * @param target
+     * @param value
+     * @return
+     */
+    public static String render(String target, Map<String, Object> value) {
+        String[] split = target.split(collectionReg.toString());
+        Matcher matcher = collectionReg.matcher(target);
+        for (String s : split) {
+            sb.append(renderString(s, value));
+            if (matcher.find()) {
+                String param = matcher.group(3);
+                if (!"".equals(nullToEmpty(param))) {
+                    putParamToMap(param);
+                }
+
+                String key = matcher.group(1);
+                String valueStr = matcher.group(2);
+                Object v;
+                if ((v = value.get(key)) instanceof List) {
+                    sb.append(renderList(valueStr, (List) v));
+                } else if ((v = value.get(key)) instanceof Object[]) {
+                    sb.append(renderList(valueStr, Arrays.asList((Object[]) v)));
+                } else if ((v = value.get(key)) instanceof Map) {
+                    sb.append(renderMap(valueStr, (Map) v));
+                }
+            }
+        }
+
+        return value();
+    }
+
+    /**
+     * 将集合内前缀后缀参数存入临时map
+     * @param param
+     */
+    public static void putParamToMap(String param) {
+        Matcher matcher = collectionArgReg.matcher(param);
+        paramMap.clear();
+        while (matcher.find()) {
+            paramMap.put(matcher.group(1), matcher.group(2));
+        }
+    }
 
 }
--- src/test/java/com/test/yang/TestSQLExecuteor/TestUtil/TestStringTemplate/TestSTemplate.java
+++ src/test/java/com/test/yang/TestSQLExecuteor/TestUtil/TestStringTemplate/TestSTemplate.java
@@ -5,7 +5,7 @@ import com.yang.SQLExecutor.util.stringTemplate.STemplate;
 import org.junit.Test;
 import sun.misc.Regexp;
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,10 +15,16 @@ import java.util.regex.Pattern;
  */
 public class TestSTemplate {
 
+
     @Test
     public void test01() {
-        String split = "select {sp} from {fp} where {wp}";
-
+        String split = "select "
+                .concat("@{sp}")
+                .concat(" from ")
+                .concat("@{fp}")
+                .concat(" where ")
+                .concat("@{wp:(index = @{i},value = @{v}).j(' and ').f(' where').l(' ; ')} order by cityName");
+        System.out.println(split);
         HashMap<String, String> hashMap = new HashMap<String, String>() {{
             put("sp", "cityName, cityCode");
             put("fp", "`city`");
@@ -26,8 +32,165 @@ public class TestSTemplate {
         }};
 
         TimeTest.testTime(() -> {
-            System.out.println(STemplate.render(split, hashMap));
+//            System.out.println(STemplate.render(split, hashMap));
         });
 
+    }
+
+    @Test
+    public void test02() {
+        Pattern mValue = Pattern.compile("\\@\\{(\\w{1,9})}");
+
+        Pattern mList = Pattern.compile("@\\{(\\w+):\\(([\\S\\s]+)\\)}");
+
+        Matcher matcher = mList.matcher("@{wp:(index = @{i},value = @{v})}");
+//        System.out.println(matcher.groupCount());
+
+        String[] arr = {"a", "b", "c", "d"};
+
+        HashMap<String, Object> map = new HashMap<>();
+
+        map.put("wp", arr);
+        while (matcher.find()) {
+            Object o = map.get(matcher.group(1));
+            List<String> value = null;
+            HashMap<String, Object> tempMap = new HashMap<>();
+            if (o instanceof String[]) {
+                value = Arrays.asList((String[]) o);
+            } else if (o instanceof List) {
+                value = (List<String>) o;
+            }
+            for (int i = 0; i < value.size(); i++) {
+                tempMap.put("i", i);
+                tempMap.put("v", value.get(i));
+                Matcher matcher1 = mValue.matcher(matcher.group(2));
+                while (matcher1.find()) {
+                    System.out.println(matcher1.group(1));
+                }
+            }
+        }
+
+    }
+
+    @Test
+    public void test03() {
+        String str = "@{wp:(index = @{i},value = @{v}).j(' and ').f(' where').l(' ; ')}";
+        Pattern compile = Pattern.compile("@\\{(\\w+):\\(([\\w =@\\{},-]+)\\)([\\S\\s]+)}");
+        Pattern compile1 = Pattern.compile("\\.([jlf])\\('([\\w ;]+)'\\)");
+        Matcher matcher = compile.matcher(str);
+        while (matcher.find()) {
+//            parseListMarcher(matcher);
+//            Matcher matcher1 = compile1.matcher(matcher.group(3));
+//            while (matcher1.find()) {
+//                System.out.println(matcher1.group(1));
+//                System.out.println(matcher1.group(2));
+//            }
+        }
+    }
+
+    @Test
+    public void test05() {
+        List<String> strings = Arrays.asList("1", "2", "3");
+        String str = "@{wp:(@{i} = @{v}).j(' and ').f(' where ').l(' ; ')}";
+        HashMap<String, Object> wp = new HashMap<String, Object>() {{
+            put("wp", strings);
+        }};
+        Pattern compile = Pattern.compile("@\\{(\\w+):\\(([\\w =@\\{},-]+)\\)([\\S\\s]+)}");
+        Matcher matcher = compile.matcher(str);
+        while (matcher.find()) {
+//            parseListMarcher(matcher, wp);
+        }
+    }
+
+    @Test
+    public void test07() {
+        String[] a1 = {"b", "c", "d"};
+        String[] a2 = new String[5];
+        a2[0] = "a";
+        a2[a2.length - 1] = "e";
+        System.arraycopy(a1, 0, a2, 1, a1.length);
+        System.out.println(Arrays.asList(a1));
+        System.out.println(Arrays.asList(a2));
+    }
+
+    @Test
+    public void test06() {
+        String s = "a";
+        System.out.println(s);
+        s.concat("b");
+        System.out.println(s);
+    }
+
+    @Test
+    public void test08() {
+        String str = "select @{sp:(@{v}).j(', ')} from `@{fp}` @{wp:(@{k} = '@{v}').f('where ').j(' and ')}";
+        HashMap<String, Object> hashMap = new HashMap<String, Object>() {
+            {
+                put("sp", Arrays.asList("CityName", "CityCode"));
+                put("fp", "city");
+                put("wp", new HashMap<String, Object>() {
+                    {
+                        put("cityName", "Beijing");
+                        put("cityCode", "PEK");
+                    }
+                });
+            }
+        };
+
+        TimeTest.testTime(() -> {
+            System.out.println(STemplate.render(str, hashMap));
+        });
+    }
+
+    @Test
+    public void test09(){
+       String sqlTemplate = "update `@{tableName}` @{sp:(@{k} = '@{v}').j(', ').f('set ')} @{wp:(@{k} = '@{v}').j(' and ').f('where ')}";
+
+        HashMap<String, Object> hashMap = new HashMap<String, Object>() {
+            {
+                put("tableName", "city");
+                put("sp", new HashMap<String, Object>(){{
+                    put("CityName", "BJ");
+                }});
+
+//                put("wp", new HashMap<String, Object>() {
+//                    {
+//                        put("cityCode", "PEK");
+//                    }
+//                });
+            }
+        };
+
+        System.out.println(STemplate.render(sqlTemplate, hashMap));
+
+    }
+
+    @Test
+    public void test10(){
+       String sql = "insert into `@{tableName}` @{colNames:(@{v}).f(' (').l(') ').j(', ')} values @{values:('@{v}').f(' (').l(') ').j(', ')}";
+
+        HashMap<String, Object> hashMap = new HashMap<String, Object>() {
+            {
+                put("tableName", "city");
+                put("colNames", Arrays.asList("CityName", "CityCode", "CountryCode"));
+                put("values", Arrays.asList("Wuhan", "TH", "CN"));
+            }
+        };
+
+
+        System.out.println(STemplate.render(sql, hashMap));
+        String s = "(?<=@{)(\\w{0,9})(?=})";
+    }
+
+    @Test
+    public void test11(){
+           String str ="@{map}.for(@{v1}.for(@{k2} @{k1} @{v2}.j(12))).j(1).f(j)";
+        HashMap<String, Object> hashMap = new HashMap<String, Object>() {
+            {
+                put("name", "city");
+                put("arr", Arrays.asList(Arrays.asList("CityName", "CityCode", "CountryCode")));
+                put("map", Arrays.asList("Wuhan", "TH", "CN"));
+            }
+        };
     }
 }
